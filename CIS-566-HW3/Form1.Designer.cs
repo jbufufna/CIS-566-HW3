6a17cd7633c1471b4d240b54b141ecf16a781422
jbufufna <83720728+jbufufna@users.noreply.github.com>
2024-12-01 20:05:56 -0500

    Rough draft

--- CIS-566-HW3/Form1.Designer.cs
+++ CIS-566-HW3/Form1.Designer.cs
@@ -45,6 +45,7 @@ namespace CIS_566_HW3
             this.button1.TabIndex = 0;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -74,7 +75,6 @@ namespace CIS_566_HW3
             this.comboBox1.Size = new System.Drawing.Size(89, 21);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.Text = "Mile";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // textBox2
             // 
--- CIS-566-HW3/Form1.cs
+++ CIS-566-HW3/Form1.cs
@@ -12,14 +12,148 @@ namespace CIS_566_HW3
 {
     public partial class Form1 : Form
     {
+        LengthConverterClient client;
+
         public Form1()
         {
             InitializeComponent();
+            client = new LengthConverterClient();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            float km_value = (float) Convert.ToDouble(textBox1.Text);
 
+            float result = client.convert_length(km_value, comboBox1.Text);
+
+            textBox2.Text = result.ToString();
         }
     }
+
+
+    public class LengthConverterClient
+    {
+        UnitConverterHandler handler;
+
+        public LengthConverterClient()
+        {
+            MileConverterHandler mileHandler = new MileConverterHandler();
+            YardConverterHandler yardHandler = new YardConverterHandler();
+
+            mileHandler.SetSuccessor(yardHandler);
+
+            FeetConverterHandler feetHandler = new FeetConverterHandler();
+
+            yardHandler.SetSuccessor(feetHandler);
+
+            handler = mileHandler;
+        }
+
+        public float convert_length(float value, string target_unit)
+        {
+            return handler.handle_request(value, target_unit);
+        }
+
+    }
+
+
+    public abstract class UnitConverterHandler
+    {
+        protected UnitConverterHandler successor;
+
+        public void SetSuccessor(UnitConverterHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public virtual float handle_request(float value, string target_unit)
+        {
+            return 0f;
+        }
+    }
+
+
+    public class MileConverterHandler : UnitConverterHandler
+    {
+
+        public override float handle_request(float value, string target_unit)
+        {
+            value *= 0.621371f; // convert KM to miles
+
+            if (target_unit == "Mile")
+            {
+                return value;
+            }
+            else
+            {
+                if (successor != null)
+                {
+                    return successor.handle_request(value, target_unit);
+                }
+                else
+                {
+                    return value;
+                }
+            }
+        }
+
+    }
+
+
+    public class YardConverterHandler : UnitConverterHandler
+    {
+
+        public override float handle_request(float value, string target_unit)
+        {
+            value *= 1760f; // convert miles to yards
+
+            if (target_unit == "Yard")
+            {
+                return value;
+            }
+            else
+            {
+                if (successor != null)
+                {
+                    return successor.handle_request(value, target_unit);
+                }
+                else
+                {
+                    return value;
+                }
+            }
+        }
+
+    }
+
+
+    public class FeetConverterHandler : UnitConverterHandler
+    {
+
+        public override float handle_request(float value, string target_unit)
+        {
+            value *= 3f; // convert yards to feet
+
+            if (target_unit == "Foot")
+            {
+                return value;
+            }
+            else
+            {
+                if (successor != null)
+                {
+                    return successor.handle_request(value, target_unit);
+                }
+                else
+                {
+                    return value;
+                }
+            }
+        }
+
+    }
+
+
+
+
 }
